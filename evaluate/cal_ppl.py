798c0aecfe1a01a682a9e6c815c3d97af3b7aaf3
Leroll <senjaer1919@gmail.com>
2026-03-27 21:38:39 +0800

    delete 过时无用代码

--- evaluate/cal_ppl.py
+++
@@ -1,6 +0,0 @@
-class CalPpl:
-    def __init__(self):
-        self.model = self.__init_model()
-
-    def __init_model(self):
-        #TODO 
--- utils/clean_nvidia_vram.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 清空nvidia gpu 显存
-# apt-get install psmisc 安装fuser
-
-fuser /dev/nvidia* | awk '{for(i=1;i<NF;i++) print "kill -9 " $i;}' | sh
